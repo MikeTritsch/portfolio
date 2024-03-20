9264be173b042ef7943b6ed130130e695d124af2
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-03-20 14:14:12 -0500

    Added entry page with background styling/sizing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+
+const Home = () => {
+  return (
+    <>
+      <div className="dark-bg"></div>
+      <div className="text-wrapper"></div>
+        <div className="letter">M</div>
+        <div className="letter">
+          <button className="enter-btn">Enter</button>
+        </div>
+    </>
+  )
+}
+
+export default Home
