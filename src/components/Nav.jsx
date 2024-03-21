eafc08fe705d83ee5604b0fdb6465f27fd5e89e9
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-03-21 16:40:10 -0500

    Added about section, need to pinpoint some final styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
   return (
     <>
-      <div className='nav-wrapper'>
-        <ul className='nav-list'>
-          <li className='nav-list-item'>About</li>
-          <li className='nav-list-item'>Web Developer</li>
-          <li className='nav-list-item'>Music</li>
-          <li className='nav-list-item'>Graphic Design</li>
-          <li className='nav-list-item'>Resume</li>
-          <li className='nav-list-item'>Contact</li>
+      <div className="nav-wrapper">
+        <ul className="nav-list">
+          <a>
+            <li className="nav-list-item">About</li>
+          </a>
+          <a>
+            <li className="nav-list-item">Projects</li>
+          </a>
+          <a>
+            <li className="nav-list-item">Music</li>
+          </a>
+          <a>
+            <li className="nav-list-item">Graphic Design</li>
+          </a>
+          <a>
+            <li className="nav-list-item">Resume</li>
+          </a>
+          <a>
+            <li className="nav-list-item">Contact</li>
+          </a>
         </ul>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import Nav from '../components/Nav';
+import About from '../components/About';
 import { Fade } from 'react-awesome-reveal';
 
 const Home = () => {
   const [showEnterButton, setShowEnterButton] = useState(true);
   const [showLetter, setShowLetter] = useState(true);
   const [showNav, setShowNav] = useState(false);
+  const [showAbout, setShowAbout] = useState(false);
 
   const handleEnterClick = () => {
     setShowEnterButton(false);
     setShowNav(true);
+    setShowAbout(true);
 
     // Fade out the background image
     const bgOverlay = document.querySelector('.background-overlay');
@@ -34,6 +37,8 @@ const Home = () => {
       <Fade>
         {showNav && (<Nav/>)};
       </Fade>
+
+      {showAbout && (<About/>)};
     </>
   );
 };
