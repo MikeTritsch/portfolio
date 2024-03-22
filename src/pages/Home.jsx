ac3df422546b09870572a8b856ff27f0a25de7ca
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-03-22 15:50:57 -0500

    Added copyright footer, with some questionable methods...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,13 @@ const Home = () => {
   const [showLetter, setShowLetter] = useState(true);
   const [showNav, setShowNav] = useState(false);
   const [showAbout, setShowAbout] = useState(false);
+  const [showFooter, setShowFooter] = useState(false);
 
   const handleEnterClick = () => {
     setShowEnterButton(false);
     setShowNav(true);
     setShowAbout(true);
+    setShowFooter(true);
 
     // Fade out the background image
     const bgOverlay = document.querySelector('.background-overlay');
@@ -37,6 +39,7 @@ const Home = () => {
       <Fade triggerOnce={true}>
         {showNav && (<Nav/>)};
       </Fade>
+      {showFooter && <footer className='footer'>© Michael Tritsch 2023</footer>}
 
       {showAbout && (<About/>)};
     </>
