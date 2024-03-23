241613aeb90455ebb073ebacc964785de4be3d07
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-03-23 12:16:20 -0500

    Fixed loading bug with nav menu and headline rendering false to the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import Nav from '../components/Nav';
-import About from '../components/About';
-import { Fade } from 'react-awesome-reveal';
+import React, { useState } from "react";
+import Nav from "../components/Nav";
+import About from "../components/About";
+import { Fade } from "react-awesome-reveal";
 
 const Home = () => {
   const [showEnterButton, setShowEnterButton] = useState(true);
   const [showLetter, setShowLetter] = useState(true);
-  const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(null);
   const [showAbout, setShowAbout] = useState(false);
   const [showFooter, setShowFooter] = useState(false);
-  const [showHeadline, setShowHeadline] = useState(false);
+  const [showHeadline, setShowHeadline] = useState(null);
 
   const handleEnterClick = () => {
     setShowEnterButton(false);
@@ -19,12 +19,12 @@ const Home = () => {
     setShowHeadline(true);
 
     // Fade out the background image
-    const bgOverlay = document.querySelector('.background-overlay');
+    const bgOverlay = document.querySelector(".background-overlay");
 
     if (bgOverlay) {
-      bgOverlay.style.transition = 'opacity 1s ease';
-      bgOverlay.style.opacity = '0';
-    };
+      bgOverlay.style.transition = "opacity 1s ease";
+      bgOverlay.style.opacity = "0";
+    }
   };
 
   return (
@@ -34,19 +34,25 @@ const Home = () => {
       </div>
       {showEnterButton && (
         <div className="btn-wrapper">
-          <button className="enter-btn" onClick={handleEnterClick}>Enter</button>
+          <button className="enter-btn" onClick={handleEnterClick}>
+            Enter
+          </button>
         </div>
       )}
       {showLetter && <div className="letter">M</div>}
       <Fade triggerOnce={true}>
-      {showHeadline && <h1 className='headline'>Full Stack Developer | Musician | Graphic Designer</h1>}
+        {showHeadline && (
+          <h1 className="headline">
+            Full Stack Developer | Musician | Creative
+          </h1>
+        )}
       </Fade>
       <Fade triggerOnce={true}>
-        {showNav && (<Nav/>)};
+        {showNav && <Nav />}
       </Fade>
-      {showFooter && <footer className='footer'>© Michael Tritsch 2023</footer>}
+      {showFooter && <footer className="footer">© Michael Tritsch 2023</footer>}
 
-      {showAbout && (<About/>)};
+      {showAbout && <About />}
     </>
   );
 };
