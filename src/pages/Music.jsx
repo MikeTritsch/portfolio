2837b64038dc1a628fbbe0a156d8b27f5c21d6e9
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-04-21 10:17:50 -0500

    Added spotify embed npm package and playlists on the music page

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,8 +1,23 @@
-
+import Nav from "../components/Nav"
+import { Spotify } from "react-spotify-embed"
 
 const Music = () => {
   return (
-    <div>Music</div>
+    <>
+      <Nav />
+        <h1 className="page-title">Music</h1>
+        
+        <div className="playlist-container">
+          <Spotify className="spotify-playlist" link="https://open.spotify.com/playlist/000EOmuzrZ7YYAnQ9NTzVD?si=918b6232d65f43ca"/>
+
+          <Spotify className="spotify-playlist" link="https://open.spotify.com/playlist/2ldcx8tqMeOqfHpkUZqJEq?si=abf8da7bc7c640b6"/>
+        </div>
+    
+    
+    
+    
+    
+    </>
   )
 }
 
